79e34b44bb0c61e0c237b1e01b441e91bb4bf9fa
Lili Peng <lili.peng@venafi.com>
2018-01-21 21:40:49 -0700

    Initial commit for fraction calculator

---
+++ LAB2/src/FractionCalc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class FractionCalc 
+{
+	private Fraction right;
+	private Fraction left;
+	private char operator;
+	
+	public FractionCalc(String input)
+	{
+		
+	}
+	
+}
