4b9b376cd3d4dcae8b49c39f40d5af3d1b679caf
Lili Peng <lili.peng@venafi.com>
2018-02-04 12:06:29 -0700

    Initial commit for lab4 and completed person class construction

---
+++ LAB4/src/Person.java
@@ -0,0 +1,56 @@
+
+public class Person implements Comparable<Person>
+{
+	private int id;
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	private String phoneNumber;
+	
+	public Person(int num, String n, String s, String c, String st, String p)
+	{
+		id = num;
+		name = n;
+		street = s;
+		city = c;
+		state = st;
+		phoneNumber = p;
+	}
+	
+	public Person(String n)
+	{
+		name = n;
+		id = 0;
+		street = null;
+		city = null;
+		state = null;
+		phoneNumber = null;
+	}
+	
+	public Person(int n)
+	{
+		id = n;
+		name = null;
+		street = null;
+		city = null;
+		state = null;
+		phoneNumber = null;
+	}
+	
+	public String toString()
+	{
+		return id + " " + name + " " + street + " " + city + " " + state + " " + phoneNumber;
+	}
+	
+	public int getID()
+	{
+		return id;
+	}
+	
+	public int compareTo(Person person)
+	{
+		return this.name.compareTo(person.name);
+	}
+
+}
