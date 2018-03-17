1174b2bd8f7147172e0a1d143f59d81de5bfe7a0
Lili Peng <lili.peng@venafi.com>
2018-03-17 14:54:31 -0600

    Completed picture tile

--- LAB5/src/PictureTile.java
+++ LAB5/src/PictureTile.java
@@ -1,11 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
 
 abstract public class PictureTile extends Tile
 {
 	private String name;
+	private ImageIcon image;
 	
 	public PictureTile(String name)
 	{
 		this.name = name;
+		image = new ImageIcon("images/" + name + ".png");
+	}
+	
+	public void paintComponent(Graphics g) 
+	{
+		super.paintComponent(g);
+		
+		setToolTipText(toString());
+		
+		int imgSize = (int)(size * .9);
+		g.drawImage(image.getImage(), faceX + size / 2 - imgSize / 2, size / 2 - imgSize / 2, imgSize, imgSize, this);	
 	}
 	
 	public String toString()
@@ -31,5 +45,29 @@ abstract public class PictureTile extends Tile
 		}
 		
 		return "";
+	}
+	
+	public static void main(String[] args)
+	{
+		JFrame	frame = new JFrame();
+
+		frame.setLayout(new FlowLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Picture Tiles");
+
+		frame.add(new Bamboo1Tile());
+
+		frame.add(new FlowerTile("Chrysanthemum"));
+		frame.add(new FlowerTile("Orchid"));
+		frame.add(new FlowerTile("Plum"));
+		frame.add(new FlowerTile("Bamboo"));
+
+		frame.add(new SeasonTile("Spring"));
+		frame.add(new SeasonTile("Summer"));
+		frame.add(new SeasonTile("Fall"));
+		frame.add(new SeasonTile("Winter"));
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
